09693e55ce308af8f5b775c487da21cdfe8bfff6
gseilerdev <guille85ok@gmail.com>
2026-06-17 15:12:04 -0300

    Tu primera variable de análisis

---
+++ clase1_ejercicio1.py
@@ -0,0 +1,11 @@
+# variables
+
+cliente = "Maria Garcia"
+edad = 34
+ultima_compra = 5420.5
+premium = True
+
+# Imprime cada variable con su etiqueta
+
+print(f"cliente = {cliente} \n edad = {edad} \n ultima_compra = {ultima_compra} \n premium = {premium}")
+
