e62711356368124e01e1590c244e8b2db4d36113
gseilerdev <guille85ok@gmail.com>
2026-06-17 22:45:11 -0300

    Detective de tipos

---
+++ clase1_ejercicio7.py
@@ -0,0 +1,17 @@
+valor_a = "42"
+valor_b = "3.14"
+valor_c = "True"
+valor_d = ""
+valor_e = "N/A"
+valor_f = "  150  "
+valor_g = "$1,250.50"
+valor_h = "01/03/2024"
+     
+valor_a_int = int(valor_a)
+valor_b_float = float(valor_b)
+valor_c_bool = valor_c == "True"
+valor_d_bool = NONE
+valor_e_bool = NONE
+valor_f_int = int(valor_f.strip())
+valor_g_float = float(valor_g.replace("$", "").replace(",", ""))
+valor_h_date = valor_h
